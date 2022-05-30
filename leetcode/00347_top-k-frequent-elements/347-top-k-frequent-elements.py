21ab7451eec6284be5af0c99b35a8fd829927a3b
kaka <vn503024@gmail.com>
2022-05-31 03:15:28 +0800

    Time: 147 ms (43.56%), Space: 19.5 MB (18.58%) - LeetHub

--- leetcode/00347_top-k-frequent-elements/347-top-k-frequent-elements.py
+++ leetcode/00347_top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,16 +1,54 @@
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         counts_map = {}
+        
+#         for num in nums:
+#             if num not in counts_map:
+#                 counts_map[num] = 1
+#             else:
+#                 counts_map[num] += 1
+        
+#         sorted_map = dict(sorted(counts_map.items(), 
+#                                  reverse=True, 
+#                                  key=lambda item: item[1]))
+        
+#         return [key for key,value in sorted_map.items()][:k]
+
+
+# Bucket Sort
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counts_map = {}
-        
+        buckets = []
+        number_count = {}
+
+        # Create empty buckets
+        for i in range(len(nums) + 1):
+            buckets.append([])
+            
+        # Put element into different buckets based on the frequence
+        ## 1. Count number, frequence
         for num in nums:
-            if num not in counts_map:
-                counts_map[num] = 1
+            if num not in number_count:
+                number_count[num] = 1
             else:
-                counts_map[num] += 1
+                number_count[num] += 1
+
+        ## 2. Put element            
+        for num, freq in number_count.items():
+            buckets[freq].append(num)
+            
+        # Sort the elements of each bucket
+        for i in range(len(buckets)):
+            buckets[i] = sorted(buckets[i])
         
-        sorted_map = dict(sorted(counts_map.items(), 
-                                 reverse=True, 
-                                 key=lambda item: item[1]))
+        flat_list = []
+        # traverse from right to left so number with higher frequency come first
+        # 是因為我們是按照 counts 來排的，所以越右邊的，出現頻率越高
+        for i in range(len(buckets) - 1, -1, -1):
+            bucket = buckets[i]
+            if bucket:
+                for num in bucket:
+                    flat_list.append(num)
         
-        return [key for key,value in sorted_map.items()][:k]
+        return flat_list[:k]
         
