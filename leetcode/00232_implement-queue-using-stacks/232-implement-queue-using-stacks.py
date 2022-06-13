34c0dec998ff7c6087170014bbba501521c4eefd
kaka <vn503024@gmail.com>
2022-06-14 01:10:57 +0800

    Time: 38 ms (64.93%), Space: 14 MB (73.71%) - LeetHub

--- leetcode/00232_implement-queue-using-stacks/232-implement-queue-using-stacks.py
+++ leetcode/00232_implement-queue-using-stacks/232-implement-queue-using-stacks.py
@@ -1,24 +1,46 @@
+# class MyQueue:
+#     def __init__(self):
+#         self.queue = []
+        
+#     def push(self, x: int) -> None:
+#         self.queue.append(x)
+
+#     def pop(self) -> int:
+#         if not self.empty():
+#             front = self.queue[0]
+#             self.queue = self.queue[1:]
+#             return front
+        
+#     def peek(self) -> int:
+#         if not self.empty():
+#             return self.queue[0]
+#         else:
+#             return None
+        
+#     def empty(self) -> bool:
+#         return True if not self.queue else False
+
+
 class MyQueue:
     def __init__(self):
-        self.queue = []
-        
-    def push(self, x: int) -> None:
-        self.queue.append(x)
+        self.input_stack = []
+        self.output_stack = []
 
-    def pop(self) -> int:
-        if not self.empty():
-            front = self.queue[0]
-            self.queue = self.queue[1:]
-            return front
+    def push(self, x: int) -> None:
+        self.input_stack.append(x)
+
+    def pop(self) -> int:    
+        self.peek()
+        return self.output_stack.pop()
         
-    def peek(self) -> int:
-        if not self.empty():
-            return self.queue[0]
-        else:
-            return None
-        
+    def peek(self) -> int:     
+        if not self.output_stack:
+            while self.input_stack:
+                self.output_stack.append(self.input_stack.pop())
+        return self.output_stack[-1]
+
     def empty(self) -> bool:
-        return True if not self.queue else False
+        return not self.input_stack and not self.output_stack
 
 
 # Your MyQueue object will be instantiated and called as such:
