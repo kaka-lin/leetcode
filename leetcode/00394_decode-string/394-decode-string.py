8418a888bd4a4a85c86b9edc195d95283f722766
kaka <vn503024@gmail.com>
2022-06-14 02:47:00 +0800

    Time: 61 ms (8.39%), Space: 13.8 MB (70.25%) - LeetHub

--- leetcode/00394_decode-string/394-decode-string.py
+++ leetcode/00394_decode-string/394-decode-string.py
@@ -1,33 +1,58 @@
+# By kaka
+# class Solution:
+#     def decodeString(self, s: str) -> str:
+#         repeated = [[]]
+#         encoded_string = []
+#         ans = ""
+        
+#         for c in s:
+#             if c == "[":
+#                 encoded_string.append([])
+#                 repeated.append([])
+#                 continue
+#             if c == "]":
+#                 k = int("".join(repeated[-2]))
+#                 string = "".join(encoded_string[-1])
+#                 ans = k * string
+                
+#                 repeated.pop(-2)
+#                 encoded_string.pop()
+                
+#                 if len(encoded_string) < 1:
+#                     encoded_string.append([])             
+#                 encoded_string[-1].append(ans) 
+#                 continue
+                
+#             if ord(c) <= 57:
+#                 repeated[-1].append(c)
+#             else:
+#                 if len(encoded_string) < 1:
+#                     encoded_string.append([]) 
+#                 encoded_string[-1].append(c)
+                    
+#         return "".join(encoded_string[-1])
+    
+    
+    
 class Solution:
-    def decodeString(self, s: str) -> str:
-        repeated = [[]]
-        encoded_string = []
-        ans = ""
+    def decodeString(self, s: str) -> str:   
+        stack = []
+        curNum = 0
+        curString = ''
         
         for c in s:
-            if c == "[":
-                encoded_string.append([])
-                repeated.append([])
-                continue
-            if c == "]":
-                k = int("".join(repeated[-2]))
-                string = "".join(encoded_string[-1])
-                ans = k * string
-                
-                repeated.pop(-2)
-                encoded_string.pop()
-                
-                if len(encoded_string) < 1:
-                    encoded_string.append([])             
-                encoded_string[-1].append(ans) 
-                continue
-                
-            if ord(c) <= 57:
-                repeated[-1].append(c)
+            if c == '[':
+                stack.append(curString)
+                stack.append(curNum)
+                curString = ''
+                curNum = 0
+            elif c == ']':
+                num = stack.pop()
+                prevString = stack.pop()
+                curString = prevString + num * curString
+            elif c.isdigit():
+                curNum = curNum * 10 + int(c)
             else:
-                if len(encoded_string) < 1:
-                    encoded_string.append([]) 
-                encoded_string[-1].append(c)
-            
-        
-        return "".join(encoded_string[-1])
+                curString += c
+                
+        return curString
