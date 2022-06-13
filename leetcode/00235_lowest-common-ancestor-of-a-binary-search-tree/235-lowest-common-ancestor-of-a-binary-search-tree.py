91c19c133a7af4adea18e9eb663305fb58eac633
kaka <vn503024@gmail.com>
2022-06-14 00:05:17 +0800

    Time: 140 ms (21.07%), Space: 18.8 MB (21.79%) - LeetHub

--- leetcode/00235_lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ leetcode/00235_lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -5,18 +5,31 @@
 #         self.left = None
 #         self.right = None
 
+# class Solution:
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+#         if not root or root == p or root == q:
+#             return root
+        
+#         left = self.lowestCommonAncestor(root.left, p, q)
+#         right = self.lowestCommonAncestor(root.right, p, q)
+        
+#         if not left:
+#             return right
+        
+#         if not right:
+#             return left
+        
+#         return root
+
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root or root == p or root == q:
-            return root
+        curr = root
         
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-        
-        if not left:
-            return right
-        
-        if not right:
-            return left
-        
-        return root
+        while curr:
+            if p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            else:
+                return curr
