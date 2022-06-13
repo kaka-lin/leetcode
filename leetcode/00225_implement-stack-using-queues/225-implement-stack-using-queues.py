67e721dbb23d7693c067fe6b6f7bd7579268c250
kaka <vn503024@gmail.com>
2022-06-14 01:39:39 +0800

    Time: 44 ms (46.62%), Space: 14.1 MB (23.20%) - LeetHub

--- leetcode/00225_implement-stack-using-queues/225-implement-stack-using-queues.py
+++ leetcode/00225_implement-stack-using-queues/225-implement-stack-using-queues.py
@@ -1,22 +1,42 @@
+# class MyStack:
+#     def __init__(self):
+#         self.queue = []
+
+#     def push(self, x: int) -> None:
+#         self.queue.append(x)
+        
+#     def pop(self) -> int:
+#         top = self.top()
+#         self.queue = self.queue[:-1]
+#         return top
+
+#     def top(self) -> int:
+#         if not self.empty():
+#             return self.queue[-1]
+
+#     def empty(self) -> bool:
+#         return not self.queue
+        
+        
+
 class MyStack:
     def __init__(self):
-        self.queue = []
+        self.queue = collections.deque()
 
-    def push(self, x: int) -> None:
-        self.queue.append(x)
+    def push(self, x):
+        q = self.queue
+        q.append(x)
+        for _ in range(len(q) - 1):
+            q.append(q.popleft())
         
-    def pop(self) -> int:
-        top = self.top()
-        self.queue = self.queue[:-1]
-        return top
+    def pop(self):
+        return self.queue.popleft()
 
-    def top(self) -> int:
-        if not self.empty():
-            return self.queue[-1]
-
-    def empty(self) -> bool:
-        return not self.queue
-        
+    def top(self):
+        return self.queue[0]
+    
+    def empty(self):
+        return not len(self.queue)
 
 
 # Your MyStack object will be instantiated and called as such:
