9cd674cc833000c0322a88607a9257eb90fece09
kaka <vn503024@gmail.com>
2023-02-08 14:06:13 +0800

    feat: added `Reverse Nodes in k-Group`

---
+++ leetcode/00025_reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # Check if we need to reverse the group
+        curr = head
+        for _ in range(k):
+            if not curr: return head
+            curr = curr.next
+
+        # Reverse the group (basic way to reverse linked list)
+        prev = None
+        curr = head
+        for _ in range(k):
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        head.next = self.reverseKGroup(curr, k)
+        return prev
